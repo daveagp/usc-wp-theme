d29e2eaa4b58c64f729b20872ce0165fe391b4b9
David Pritchard <dpritcha@parallel05.usc.edu>
2014-08-26 20:36:00 -0700

    Initial commit

---
+++ functions.php
@@ -0,0 +1,29 @@
+<?php
+  if ( !isset( $content_width ) ) $content_width = 768;
+  if ( function_exists( 'add_theme_support' ) ) add_theme_support( 'automatic-feed-links' );
+  if ( function_exists( 'register_nav_menu' ) ) register_nav_menu( 'menu', 'Menu' );
+  if ( function_exists('register_sidebar') ) register_sidebar( array(
+    'name' => __( 'Widgets', 'simplest' ),
+    'id' => 'widgets',
+    'before_widget' => '<div class="widget">',
+    'after_widget' => '</div><!-- widget -->',
+    'before_title' => '<h4>',
+    'after_title' => '</h4>') );
+
+function autolink_emails($content) {
+  $content = preg_replace("%(?<!mailto:)\b([a-z0-9A-Z.]+)@usc.edu(?!</a>)%", 
+                          "<a href='mailto:$1@usc.edu'>$1@</a>", $content);
+  return $content;
+}
+
+add_filter( 'the_content', 'autolink_emails' );
+
+function nonbreaking_hyphens($content) {
+  $content = str_replace("&ndash;", "&#8209;", $content);
+  $content = str_replace(json_decode('"\u2013"'), "&#8209;", $content);
+  return $content;
+}
+
+add_filter( 'the_content', 'nonbreaking_hyphens');
+
+?>
---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+ <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <title><?php bloginfo( 'name' ); ?><?php wp_title( '&mdash;' ); ?></title>
+
+    <?php if ( is_singular() && get_option( 'thread_comments') ) wp_enqueue_script( 'comment-reply' ); ?>
+    <?php wp_head(); ?>
+
+   <meta name="viewport" content="width=device-width, initial-scale=0.9, user-scalable=no" />
+
+<?php $themedir =  get_template_directory_uri(); ?>
+
+   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+
+   <script src="<?php echo $themedir; ?>/js/jquery-scrolltofixed-min.js"></script>
+   <script type="text/javascript">
+     jQuery(document).ready(function() {
+         jQuery('#sidebar').scrollToFixed( { 
+     marginTop: <?php echo is_admin_bar_showing() ? 70+32 : 70; ?>,
+     fixed: function() { jQuery(this).css("box-sizing", "inherit"); },
+     unfixed: function() { jQuery(this).css("box-sizing", "border-box"); }
+         });
+     });
+     </script>
+
+<style type="text/css">
+#logo_wrapper #logo {
+    background: #990000 url("<?php echo $themedir; ?>/img/logo.gif") no-repeat;
+}
+header {
+    background: url("<?php echo $themedir; ?>/img/bg.jpg");
+}
+</style>
+
+  </head>
+  <body <?php body_class(); ?>>
+    <div id="top_bar" class="noprint">
+      <div class="center">
+	<div id="logo_wrapper">
+	  <a href="http://usc.edu"><div id="logo"></div></a>
+	</div>
+      </div>
+    </div>
+    <header class="noprint">
+      <div class="center">
+        <h1>
+	  <span><?php bloginfo( 'name' ); ?></span>
+	  <?php bloginfo( 'description' ); ?>
+	</h1>
+      </div>
+    </header>
+    
+    <div id="content" class="center">
+      <div class="noscreen">
+        <h1>
+	  <?php bloginfo( 'name' ); ?>:
+	  <?php bloginfo( 'description' ); ?>
+	</h1>
+      </div>
+      <div id="main">
+	<div class="mobileonly-block">
+	  <a href="#navigation">Jump to navigation menu</a>
+	</div>
+
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+          <!--<div <?php post_class(); ?>>-->
+            <!--<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>--> 
+	    <h2><?php the_title(); ?></h2> 
+            <?php the_content(); ?>
+            <?php if ( !is_singular() && get_the_title() == '' ) : ?>
+              <a href="<?php the_permalink(); ?>">(more...)</a>
+            <?php endif; ?>
+            <?php if ( is_singular() ) : ?>
+              <div class="pagination"><?php wp_link_pages(); ?></div>
+            <?php endif; ?>
+<!--            <div class="clear"> </div> -->
+      <!--</div><!-- post_class() -->
+          <?php if ( is_singular() ) : ?>
+            <div class="meta">
+<!--              <p>Posted by <?php the_author_posts_link(); ?>
+              on <a href="<?php the_permalink(); ?>"><?php the_date(); ?></a>
+              in <?php the_category( ', ' ); ?><?php the_tags( ', ' ); ?></p> -->
+            </div><!-- meta -->
+            <?php comments_template(); ?>
+          <?php endif; ?>
+        <?php endwhile; else: ?>
+          <div class="hentry"><h2>Sorry, the page you requested cannot be found</h2></div>
+        <?php endif; ?>
+        <?php if ( is_active_sidebar( 'widgets' ) ) : ?>
+          <div class="widgets"><?php dynamic_sidebar( 'widgets' ); ?></div>
+        <?php endif; ?>
+        <?php if ( is_singular() || is_404() ) : ?>
+<!--          <div class="left"><a href="<?php bloginfo( 'url' ); ?>">&laquo; Home page</a></div> -->
+        <?php else : ?>
+<!--          <div class="left"><?php next_posts_link( '&laquo; Older posts' ); ?></div>
+          <div class="right"><?php previous_posts_link( 'Newer posts &raquo;' ); ?></div>-->
+        <?php endif; ?>
+      </div><!-- main -->
+
+    <div id="sidebar" style="z-index: 1000; box-sizing: border-box;" class="noprint">
+      <nav id="navigation">
+	<?php wp_nav_menu(); ?>
+      </nav>
+    </div>
+
+       <div class="clear"> </div
+<footer>
+  <div class="left">&nbsp;</div>
+  <div class="right">&nbsp;</div>
+  <div class="clear"></div>
+  </footer>
+
+    </div><!-- content -->
+    <?php wp_footer(); ?> 
+  </body>
+</html>
